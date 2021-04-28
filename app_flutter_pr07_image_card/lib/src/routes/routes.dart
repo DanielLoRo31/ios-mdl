62a60b0fcfab584861d498c21ae24adf46f1db68
DanielLoRo31 <daniel.lozano.rodriguez2000@gmail.com>
2021-04-28 17:45:22 -0500

    add: animated input

--- app_flutter_pr07_image_card/lib/src/routes/routes.dart
+++ app_flutter_pr07_image_card/lib/src/routes/routes.dart
@@ -1,3 +1,6 @@
+import 'package:componentes/src/pages/animated_container.dart';
+import 'package:componentes/src/pages/animated_input_page.dart';
+import 'package:componentes/src/pages/input_page.dart';
 import 'package:flutter/material.dart';
 
 import '../pages/alert_page.dart';
@@ -11,5 +14,8 @@ Map <String, WidgetBuilder> getAplicationRoutes(){
         'alert' : (BuildContext context) => AlertPage(),
         'avatar' : (BuildContext context) => AvatarPage(),
         'card': (BuildContext context) => CardPage(),
+        'animatedContainer': (BuildContext context) => AnimatedContainerPage(),
+        'inputs': (BuildContext context) => InputPage(),
+        'inputsAnimated': (BuildContext context) => AnimatedInputPage()
       };
   }
